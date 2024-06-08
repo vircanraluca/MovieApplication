7d053097a6af82ad5574adc9a89b61c9cc10a523
vircanraluca <vircan.raluca@yahoo.com>
2024-06-08 13:51:57 +0300

    Functionalitatea de LOG IN/LOG OUT

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,11 +3,6 @@
     <v-layout>
       <v-navigation-drawer expand-on-hover rail>
         <v-list>
-          <!-- <v-list-item
-            prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
-            subtitle="sandra_a88@gmailcom"
-            title="Sandra Adams"
-          ></v-list-item> -->
           <v-list-item title="MOVIEW"></v-list-item>
         </v-list>
 
@@ -19,10 +14,18 @@
             value="home"
           ></v-list-item>
           <v-list-item
+            v-if="!isLoggedIn"
             to="/login"
             prepend-icon="mdi-login"
             title="Login"
             value="login"
+          ></v-list-item>
+          <v-list-item
+            v-if="!isLoggedIn"
+            to="/register"
+            prepend-icon="mdi-account-plus"
+            title="Register"
+            value="register"
           ></v-list-item>
           <v-list-item
             to="/movies"
@@ -31,6 +34,7 @@
             value="movies"
           ></v-list-item>
           <v-list-item
+            v-if="isLoggedIn"
             to="/favorites"
             prepend-icon="mdi-heart"
             title="Favorites"
@@ -41,17 +45,12 @@
             prepend-icon="mdi-information"
             title="About"
             value="about"
-          >
-          </v-list-item>
-          <v-list-item
-            to="/register"
-            prepend-icon="mdi-account-plus"
-            title="Register"
-            value="register"
           ></v-list-item>
           <v-list-item
+            v-if="isLoggedIn"
             prepend-icon="mdi-logout"
             title="Logout"
+            @click="handleSignOut"
             value="logout"
           ></v-list-item>
         </v-list>
@@ -63,4 +62,38 @@
     </v-layout>
   </v-card>
 </template>
-<script></script>
+
+<script>
+import { ref } from "vue";
+import { getAuth, signOut } from "firebase/auth";
+import { useRouter } from "vue-router";
+
+export default {
+  setup() {
+    const auth = getAuth();
+    const isLoggedIn = ref(false);
+    const router = useRouter();
+
+    auth.onAuthStateChanged((user) => {
+      isLoggedIn.value = !!user;
+    });
+
+    const handleSignOut = async () => {
+      try {
+        await signOut(auth);
+        isLoggedIn.value = false;
+        console.log("User signed out!");
+        router.push("/");
+      } catch (error) {
+        console.error(error.code);
+        alert(error.message);
+      }
+    };
+
+    return {
+      isLoggedIn,
+      handleSignOut,
+    };
+  },
+};
+</script>
